af9a63efc7fd90172cf3d8cbe9a7b3b7bd02934c
Chely Ho <adept_inept@yahoo.com>
2018-02-06 22:31:00 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,12 @@
 def square_array(array)
- count = 0
-  array.each do |num|
-    array[count] = num ** 2
-    count +=1
+ #count = 0
+  #array.each do |num|
+    #array[count] = num ** 2
+    #count +=1
+    
+  #end
+
+  array.collect do |num|
+    num ** 2
   end
 end
