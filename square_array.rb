3b5c22044177c07640ef83e790c1af83517dcd9b
Chely Ho <adept_inept@yahoo.com>
2018-02-06 22:32:13 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+ count = 0
+  array.each do |num|
+    array[count] = num ** 2
+    count +=1
+  end
+
+  # array.collect do |num|
+    # num ** 2
+  # end
 end
